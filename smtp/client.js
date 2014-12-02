232c63798b7460a81ac5c49237e536a268f6c6fd
antonio <antonio@dots.de>
2014-12-02 10:05:08 +0100

    bugfix issue #113: callback called multiple times with error
    response listener was called twice when the socket is already closed
    and the state is not connected.
    
    revert changes in commit 23832df015787f95933fae5672f847dc23057791

--- smtp/client.js
+++ smtp/client.js
@@ -35,12 +35,6 @@ Client.prototype =
          self.timer = setTimeout(function() { self.smtp.quit(); }, 1000);
    },
 
-   _isMessageInQueue: function(messageId) {
-     return this.queue.some(function(item) {
-       return item.message.header['message-id'] === messageId;
-     });
-   },
-
    _connect: function(stack)
    {
       var self = this,
@@ -72,9 +66,7 @@ Client.prototype =
                self.smtp.ehlo_or_helo_if_needed(begin);
          }
          else {
-           if (self._isMessageInQueue(stack.message.header['message-id'])) {
-             stack.callback(err, stack.message);
-           }
+            stack.callback(err, stack.message);
 
             // clear out the queue so all callbacks can be called with the same error message
             self.queue.shift();
