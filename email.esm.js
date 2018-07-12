ca3aab4a7994871cb52eac722988a43b3c5bf94f
Zack Schuster <email@zackschuster.com>
2018-07-12 11:44:55 -0700

    add & test single-file bundle

---
+++ email.esm.js
@@ -0,0 +1,7 @@
+import * as server from './smtp/client';
+import * as message from './smtp/message';
+import * as date from './smtp/date';
+import * as SMTP from './smtp/smtp';
+import * as error from './smtp/error';
+
+export { server, message, date, SMTP, error };
