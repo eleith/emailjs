6a1a70771188bae1827ebefc972fe8bce4ee209a
Zack Schuster <email@zackschuster.com>
2020-05-27 04:14:47 -0700

    test/auth: add no authentication unit

--- test/auth.ts
+++ test/auth.ts
@@ -30,6 +30,38 @@ let server: smtp.SMTPServer | null = null;
 
 test.afterEach.cb((t) => server?.close(t.end));
 
+test.cb('no authentication should succeed', (t) => {
+	const msg = {
+		subject: 'this is a test TEXT message from emailjs',
+		from: 'piglet@gmail.com',
+		to: 'pooh@gmail.com',
+		text: "It is hard to be brave when you're only a Very Small Animal.",
+	};
+	server = new smtp.SMTPServer({
+		authMethods: [],
+		authOptional: true,
+		onData(stream, _session, callback: () => void) {
+			mailparser
+				.simpleParser(stream)
+				.then((mail) => {
+					t.is(mail.text, msg.text + '\n\n\n');
+					t.is(mail.subject, msg.subject);
+					t.is(mail.from?.text, msg.from);
+					t.is(mail.to?.text, msg.to);
+				})
+				.finally(t.end);
+			stream.on('end', callback);
+		},
+	});
+	server.listen(port, () => {
+		new c.Client({ port }).send(new m.Message(msg), (err) => {
+			if (err) {
+				throw err;
+			}
+		});
+	});
+});
+
 test.cb('PLAIN authentication should succeed', (t) => {
 	const msg = {
 		subject: 'this is a test TEXT message from emailjs',
