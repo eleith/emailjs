8a623452520d983581449f008bc2d28144fcb4fc
Zack Schuster <email@zackschuster.com>
2020-05-27 05:35:36 -0700

    email: add response module to public api

--- email.ts
+++ email.ts
@@ -1,5 +1,6 @@
 export * from './smtp/client';
 export * from './smtp/connection';
-export * from './smtp/message';
 export * from './smtp/date';
 export * from './smtp/error';
+export * from './smtp/message';
+export * from './smtp/response';
