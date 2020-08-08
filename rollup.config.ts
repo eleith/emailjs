2b046d37160070a23b8c1de58a4c1592c75da9e9
Zack Schuster <email@zackschuster.com>
2020-08-08 12:35:12 -0700

    build: remove unused rollup plugins

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,4 @@
 import module from 'module';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
 export default {
@@ -23,8 +21,6 @@ export default {
 	],
 	external: module.builtinModules,
 	plugins: [
-		resolve(),
-		commonjs(),
 		typescript({ removeComments: false, include: ['email.ts', 'smtp/**/*'] }),
 	],
 };
